ae441cf4c24871a036a27fdd8c76bee48ce5ac53
kallisons <kallisonsmith@yahoo.com>
2015-12-09 19:04:01 -0800

    modify code to include deltaH=0
    Also, automated model input

--- sh/archive/CESM1/CESM1_dH0_p50depth_rcp8.5.sh
+++ sh/archive/CESM1/CESM1_dH0_p50depth_rcp8.5.sh
@@ -3,13 +3,13 @@ rm p50depth.jnl
 
 while IFS=, read -r species p50 deltaH
 do
-  
-  mkdir /Data/Projects/CMIP5_p50/WOA
-  mkdir /Data/Projects/CMIP5_p50/WOA/${species}
-  mkdir /Data/Projects/CMIP5_p50/WOA/${species}/p50
-  mkdir /Data/Projects/CMIP5_p50/WOA/${species}/p50depth
 
-  echo "SET DATA \"/Data/WOA/WOA_o2/WOA_po2_monthly_1deg.nc\", \"/Data/WOA/WOA_temp/temperature_monthly_1deg.nc\"" >p50depth.jnl
+  mkdir /Data/Projects/CMIP5_p50/CESM1/${species}
+  mkdir /Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0	
+  mkdir /Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50
+  mkdir /Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50depth
+
+  echo "SET DATA \"/Data/CMIP5/climdiff/WOA_modeldiff_rcp8.5/CESM1_rcp8.5_po2.nc\", \"/Data/CMIP5/climdiff/WOA_modeldiff_rcp8.5/CESM1_rcp8.5_temp.nc\"" >p50depth.jnl
 
   echo "Let p50_critter = ${p50}" >> p50depth.jnl #kPa
 
@@ -17,7 +17,7 @@ do
 
   echo "Let R =  0.008314" >> p50depth.jnl  #kJ mol^-1 K^-1 universal gas constant
 
-  echo "Let tempK = t_an[d=2] + 273.15" >> p50depth.jnl
+  echo "Let tempK = temp_rcp[d=2] + 273.15" >> p50depth.jnl
 
   echo "Let tempK_ml = tempK[z=10]" >> p50depth.jnl
 
@@ -29,16 +29,9 @@ do
 
   echo "Let p50depth = p50_diff[z=@loc:0]" >> p50depth.jnl
 
-  echo "Let p50_diff_deltaH0 = po2[d=1]-p50_critter" >> p50depth.jnl
-
-  echo "Let p50depth_deltaH0 = p50_diff_deltaH0[z=@loc:0]" >> p50depth.jnl
-
   echo "Let p50av = p50[l=@ave]" >> p50depth.jnl
 
   echo "Let p50depthav = p50depth[l=@ave]" >> p50depth.jnl
-
-  echo "Let p50depthav_deltaH0 = p50depth_deltaH0[l=@ave]" >> p50depth.jnl
-
 
   echo "define att p50.long_name = \"50% Blood Oxygenation\"" >> p50depth.jnl
 
@@ -50,18 +43,6 @@ do
 
   echo "define att p50.species_deltaH = \"${deltaH}\"" >> p50depth.jnl
 
-
-  echo "define att p50depth_deltaH0.long_name = \"Depth of P50 Threshold when deltaH=0\"" >> p50depth.jnl
-
-  echo "define att p50depth_deltaH0.units = \"m\"" >> p50depth.jnl
-
-  echo "define att p50depth_deltaH0.species = \"${species}\"" >> p50depth.jnl
-
-  echo "define att p50depth_deltaH0.species_p50 = \"${p50}\"" >> p50depth.jnl
-
-  echo "define att p50depth_deltaH0.species_deltaH = \"0\"" >> p50depth.jnl
-
-
   echo "define att p50depth.long_name = \"Depth of P50 Threshold\"" >> p50depth.jnl
 
   echo "define att p50depth.units = \"m\"" >> p50depth.jnl
@@ -71,7 +52,6 @@ do
   echo "define att p50depth.species_p50 = \"${p50}\"" >> p50depth.jnl
 
   echo "define att p50depth.species_deltaH = \"${deltaH}\"" >> p50depth.jnl
-
 
   echo "define att p50av.long_name = \"50% Blood Oxygenation\"" >> p50depth.jnl
 
@@ -83,7 +63,6 @@ do
 
   echo "define att p50av.species_deltaH = \"${deltaH}\"" >> p50depth.jnl
 
-
   echo "define att p50depthav.long_name = \"Depth of P50 Threshold\"" >> p50depth.jnl
 
   echo "define att p50depthav.units = \"m\"" >> p50depth.jnl
@@ -94,31 +73,19 @@ do
 
   echo "define att p50depthav.species_deltaH = \"${deltaH}\"" >> p50depth.jnl
 
-
-  echo "define att p50depthav_deltaH0.long_name = \"Depth of P50 Threshold when deltaH=0\"" >> p50depth.jnl
-
-  echo "define att p50depthav_deltaH0.units = \"m\"" >> p50depth.jnl
-
-  echo "define att p50depthav_deltaH0.species = \"${species}\"" >> p50depth.jnl
-
-  echo "define att p50depthav_deltaH0.species_p50 = \"${p50}\"" >> p50depth.jnl
-
-  echo "define att p50depthav_deltaH0.species_deltaH = \"0\"" >> p50depth.jnl
-
-
   echo "Set memory/size=200" >> p50depth.jnl
 
-  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50/woa.p50.${species}.nc\"/LLIMITS=1:12/L=1 p50" >> p50depth.jnl
+  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50/cesm1.rcp85.p50.${species}.nc\"/LLIMITS=1:12/L=1 p50" >> p50depth.jnl
 
-  echo "repeat/L=2:12 (SAVE/APPEND/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50/woa.p50.${species}.nc\"/L=\`l\` p50)" >> p50depth.jnl
+  echo "repeat/L=2:12 (SAVE/APPEND/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50/cesm1.rcp85.p50.${species}.nc\"/L=\`l\` p50)" >> p50depth.jnl
 
-  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50depth/woa.p50depth.${species}.nc\"/LLIMITS=1:12/L=1 p50depth, p50depth_deltaH0" >> p50depth.jnl
+  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50depth/cesm1.rcp85.p50depth.${species}.nc\"/LLIMITS=1:12/L=1 p50depth" >> p50depth.jnl
 
-  echo "repeat/L=2:12 (SAVE/APPEND/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50depth/woa.p50depth.${species}.nc\"/L=\`l\` p50depth, p50depth_deltaH0)" >> p50depth.jnl
+  echo "repeat/L=2:12 (SAVE/APPEND/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50depth/cesm1.rcp85.p50depth.${species}.nc\"/L=\`l\` p50depth)" >> p50depth.jnl
 
-  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50/woa.p50av.${species}.nc\" p50av" >> p50depth.jnl
+  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50/cesm1.rcp85.p50av.${species}.nc\" p50av" >> p50depth.jnl
 
-  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/WOA/${species}/p50depth/woa.p50depthav.${species}.nc\" p50depthav, p50depthav_deltaH0" >> p50depth.jnl
+  echo "SAVE/CLOBBER/FILE=\"/Data/Projects/CMIP5_p50/CESM1/${species}/temp_deltaH0/p50depth/cesm1.rcp85.p50depthav.${species}.nc\" p50depthav" >> p50depth.jnl
 
   echo "quit" >> p50depth.jnl
 
