053503611ac11eacfa070aad2f2a7422dba21831
kallisons <kallisonsmith@yahoo.com>
2016-02-04 12:17:24 -0800

    add p50 depth to plots

--- RCode/Transect_p50.R
+++ RCode/Transect_p50.R
@@ -35,8 +35,30 @@ trans.lon<-207.5
 	colnames(data.woa.Tm)<-depths.woa.Tm*-1
 	data.woa.Tm<-data.woa.Tm[,ncol(data.woa.Tm):1]
 	
+	file.woa.To.depth<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_obesus/p50depth/woa.p50depthav.Thunnus_obesus.nc", sep="")
+	nc.woa.To.depth<-nc_open(paste(file.woa.To.depth, sep=""))
+	lons.woa.To.depth<-nc.woa.To.depth$dim$LON$vals	
+	lats.woa.To.depth<-nc.woa.To.depth$dim$LAT$vals	
+	data.woa.To.depth<-ncvar_get(nc.woa.To.depth, nc.woa.To.depth$var[[1]], start=c(which(lons.woa.To.depth==trans.lon),1), count=c(1, 180))
+	nc_close(nc.woa.To.depth)
+	names(data.woa.To.depth)<-lats.woa.To.depth
+	data.woa.To.depth2<-data.woa.To.depth*-1
 
-	quartz(width=4, height=6)
+	
+	file.woa.Tm.depth<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_maccoyii/p50depth/woa.p50depthav.Thunnus_maccoyii.nc", sep="")
+	nc.woa.Tm.depth<-nc_open(paste(file.woa.Tm.depth, sep=""))
+	lons.woa.Tm.depth<-nc.woa.Tm.depth$dim$LON$vals	
+	lats.woa.Tm.depth<-nc.woa.Tm.depth$dim$LAT$vals	
+	data.woa.Tm.depth<-ncvar_get(nc.woa.Tm.depth, nc.woa.Tm.depth$var[[1]], start=c(which(lons.woa.Tm.depth==trans.lon),1), count=c(1, 180))
+	nc_close(nc.woa.Tm.depth)
+	names(data.woa.Tm.depth)<-lats.woa.Tm.depth
+	data.woa.Tm.depth2<-data.woa.Tm.depth*-1
+
+	
+	
+	
+
+	quartz(width=5, height=7)
 	par(plt = c(0.17,0.75,0.60,0.95), #c(left, right, bottom, top)  
     las = 1,                      # orientation of axis labels
     cex.axis = 1,                 # size of axis annotation
@@ -46,7 +68,12 @@ trans.lon<-207.5
 
 #	filled.contour3(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)), ylab="Depth (m)", xlab="", lwd=1)
 	
-	filled.contour3(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,10), color.palette=viridis, ylab="Depth (m)", xlab="", lwd=1)
+	filled.contour3(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, xlim=c(-70, 70), ylim=c(-1500,0), zlim=c(0,10), nlevels=10, color.palette=viridis, axes=FALSE, ylab="Depth (m)", xlab="", lwd=1)
+	axis(side=1, at=seq(-70,70,20), labels=FALSE)
+	axis(side=2, at=seq(-1600,0,200), labels=TRUE)
+	axis(side=3, at=c(-70,70), lwd.ticks=0, labels=FALSE)
+	axis(side=4, at=c(-1500,0), lwd.ticks=0, labels=FALSE)
+	lines(as.numeric(names(data.woa.To.depth)), data.woa.To.depth2, col="white", lwd=2.5)
 	
 	par(new = "TRUE",
     plt = c(0.8,0.85,0.60,0.95),   # define plot region for legend
@@ -56,7 +83,8 @@ trans.lon<-207.5
 
 #filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z= data.woa.To, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)))
 
-filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z= data.woa.To, zlim=c(0,10), color.palette=viridis)
+filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,10), nlevels=10, color.palette=viridis)
+filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,10), nlevels=10, color.palette=viridis)
 
 
 	par(new = "TRUE",
@@ -69,7 +97,12 @@ filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.wo
 
 #	filled.contour3(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)), ylab="Depth (m)", xlab="", lwd=1)
 
-	filled.contour3(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color.palette=viridis, ylab="Depth (m)", xlab="", lwd=1)
+	filled.contour3(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, xlim=c(-70, 70), ylim=c(-1500,0), zlim=c(0,10), nlevels=10, color.palette=viridis, axes=FALSE, ylab="Depth (m)", xlab="Degrees Latitude", lwd=1)
+	axis(side=1, at=seq(-70,70,20), labels=TRUE)
+	axis(side=2, at=seq(-1600,0,200), labels=TRUE)
+	axis(side=3, at=c(-70,70), lwd.ticks=0, labels=FALSE)
+	axis(side=4, at=c(-1500,0), lwd.ticks=0, labels=FALSE)
+	lines(as.numeric(names(data.woa.Tm.depth)), data.woa.Tm.depth2, col="white", lwd=2.5)
 	
 	par(new = "TRUE",
     plt = c(0.8,0.85,0.15,0.5),   # define plot region for legend
@@ -79,58 +112,6 @@ filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.wo
 
 #	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z= data.woa.Tm, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)))
 
-	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z= data.woa.Tm, zlim=c(0,10), color.palette=viridis)
-
-
-	
-	
-	
-	
-		data2<-as.vector(data)
-		data3<-subset(data2, is.na(data2)==FALSE)
-		if(a==1){
-			depthtable<-as.matrix(data3)
-			colnames(depthtable)<-specieslist[a]		
-		}else{
-			if(nrow(depthtable)>length(data3)){
-				data3<-c(data3, rep(NA, 1, nrow(depthtable)-length(data3)))					
-			}else{
-				addNA<-matrix(NA, length(data3)-nrow(depthtable), ncol(depthtable))
-				depthtable<-rbind(depthtable, addNA)				
-			}
-			depthtable<-cbind(depthtable, data3)
-			colnames(depthtable)[a]<-paste(specieslist[a])
-		}	
-		
-	}
-depthtable<-depthtable*-1
-
-
-
-
-quartz(height=8, width=4)
-#outfile<-paste("~/Code/Projects/CMIP5_p50/graphs/WOA_geostats.ps")
-#postscript(outfile, height=8, width=4)
-par(mfrow=c(3,1))
-par(mar=c(1, 4.5, 0, 0.5))
-par(oma=c(2, 0, 2, 1))
-par(las=1)
-par(yaxs="i")
-
-  barplot(woa, ylim=c(0, 200), col=c("black", "white"), legend=c(expression(paste(Delta, "H", "\U2260", "0 (Effect of Temperature)", sep=""), paste(Delta, "H", "\U003D", "0 (No Effect of Temperature)", sep=""))), args.legend = list(x = "topleft", inset=c(0.05, 0.02), cex=1), xaxt="n", ylab=expression(paste("area (10"^"6", " km"^"2", ")", sep="")), space=c(0,0.8), beside=TRUE)
-  box()
-  abline(h=0, lwd=2)
-  
-  collist<-c("black", "white","", "black", "white", "", "black", "white", "", "black", "white", "", "black", "white", "", "black", "white")
-  medcollist<-c("white", "black", "","white", "black", "","white", "black", "","white", "black", "","white", "black", "","white", "black")
-  boxplot(cdepthtable, ylim=c(-1500, 0), xaxt="n", ylab="depth (m)", col=collist, medcol=medcollist, notch=TRUE, outline=FALSE)
-  locs<-c(1,2,4,5,7,8,10,11,13,14,16,17)
-  axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-  
-  boxplot(cdepthtable2, ylim=c(-1500, 0), xaxt="n", col=collist, medcol=medcollist, ylab="depth (m) for common areas", notch=TRUE, outline=FALSE)
-  axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-  specieslist<-c("Thunnus\nobesus", "Thunnus\nalbacares", "Katsuwonus\npelamis", "Thunnus\nalalunga", "Thunnus\nthynnus",  "Thunnus\nmaccoyii")
-  axis(side=1, at=c(1.5,4.5,7.5,10.5,13.5,16.5), line=-0.5, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8)
-  mtext("WOA P50Depth", adj=0.6, outer=TRUE)
-dev.off()
+	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color=viridis, nlevels=10)
+	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color=viridis, nlevels=10)
 
