0aec5f378c0a5d3168144f64a60d187c974db70d
kallisons <kallisonsmith@yahoo.com>
2016-03-22 13:58:34 -0700

    update IUCN .sh file

--- archive/IUCN_Curtis_shptonc.sh
+++ archive/IUCN_Curtis_shptonc.sh
@@ -17,23 +17,3 @@ mv Birds/gmt/*.nc Birds/nc
 
 ncrename -vz,mask Birds/nc/*.nc
 
-
-
-gmt grdmask -Rglobal_land_tmp.nc?atmat -f0x -f1y -NNaN/1/1 Birds/gmt/Acrocephalus_arundinaceus_22714745.gmt -GBirds/nc/Acrocephalus_arundinaceus_22714745.nc
-
-
-
-
-ncrename -vz,mask ${folder}species.nc
-cp ${folder}species.nc "${folder}IUCN_${filename}.nc"
-done
-
-
-ogr2ogr -f "GMT" -dim 3 gmt/Zosterops_xanthochroa_22714246.gmt shp/Zosterops_xanthochroa_22714246.shp
-
-gmt grdmask -R-179.5/179.5/-89.5/89.5 -I1 -f0x -f1y -NNaN/1/1 gmt/Zosterops_xanthochroa_22714246.gmt -Gnc/^^_22714246.nc
-
-ncrename -vz,mask Zosterops_xanthochroa_22714246.nc
-
-
-ogrinfo -al shp/Zosterops_xanthochroa_22714246.shp
