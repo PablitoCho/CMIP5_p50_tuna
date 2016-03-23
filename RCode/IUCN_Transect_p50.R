04cfc11ced21d959bbaa977bfebf395fcaf5c876
kallisons <kallisonsmith@yahoo.com>
2016-03-23 09:51:51 -0700

    Update IUCN R Code

---
+++ RCode/IUCN_Transect_p50.R
@@ -0,0 +1,153 @@
+library(RNetCDF)
+library(ncdf4)
+library(viridis)
+
+source('~/Code/Projects/CMIP5_p50/RCode/filled.contour/filled.contour.R', chdir = TRUE)
+source('~/Code/Projects/CMIP5_p50/RCode/filled.contour/filled.legend.R', chdir = TRUE)
+
+trans.lon<-207.5
+
+#-------------------------
+# P50 WOA
+#-------------------------
+		
+	file.woa.To<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_obesus/p50/woa.p50av.Thunnus_obesus.nc", sep="")
+	nc.woa.To<-nc_open(paste(file.woa.To, sep=""))
+	lons.woa.To<-nc.woa.To$dim$LON$vals	
+	lats.woa.To<-nc.woa.To$dim$LAT$vals	
+	depths.woa.To<-nc.woa.To$dim$DEPTH$vals	
+	data.woa.To<-ncvar_get(nc.woa.To, nc.woa.To$var[[2]], start=c(which(lons.woa.To==trans.lon),1,1), count=c(1, 180, 24))
+	nc_close(nc.woa.To)
+	
+	rownames(data.woa.To)<-lats.woa.To
+	colnames(data.woa.To)<-depths.woa.To*-1
+	data.woa.To<-data.woa.To[,ncol(data.woa.To):1]
+	
+	file.woa.Tm<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_orientalis/p50/woa.p50av.Thunnus_orientalis.nc", sep="")
+	nc.woa.Tm<-nc_open(paste(file.woa.Tm, sep=""))
+	lons.woa.Tm<-nc.woa.Tm$dim$LON$vals	
+	lats.woa.Tm<-nc.woa.Tm$dim$LAT$vals	
+	depths.woa.Tm<-nc.woa.Tm$dim$DEPTH$vals	
+	data.woa.Tm<-ncvar_get(nc.woa.Tm, nc.woa.Tm$var[[2]], start=c(which(lons.woa.Tm==trans.lon),1,1), count=c(1, 180, 24))
+	nc_close(nc.woa.Tm)
+	
+	rownames(data.woa.Tm)<-lats.woa.Tm
+	colnames(data.woa.Tm)<-depths.woa.Tm*-1
+	data.woa.Tm<-data.woa.Tm[,ncol(data.woa.Tm):1]
+	
+	file.woa.To.depth<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_obesus/p50depth/woa.p50depthav.Thunnus_obesus.nc", sep="")
+	nc.woa.To.depth<-nc_open(paste(file.woa.To.depth, sep=""))
+	lons.woa.To.depth<-nc.woa.To.depth$dim$LON$vals	
+	lats.woa.To.depth<-nc.woa.To.depth$dim$LAT$vals	
+	data.woa.To.depth<-ncvar_get(nc.woa.To.depth, nc.woa.To.depth$var[[1]], start=c(which(lons.woa.To.depth==trans.lon),1), count=c(1, 180))
+	nc_close(nc.woa.To.depth)
+	names(data.woa.To.depth)<-lats.woa.To.depth
+	data.woa.To.depth2<-data.woa.To.depth*-1
+
+	
+	file.woa.Tm.depth<-paste("/Data/Projects/CMIP5_p50/WOA/Thunnus_orientalis/p50depth/woa.p50depthav.Thunnus_orientalis.nc", sep="")
+	nc.woa.Tm.depth<-nc_open(paste(file.woa.Tm.depth, sep=""))
+	lons.woa.Tm.depth<-nc.woa.Tm.depth$dim$LON$vals	
+	lats.woa.Tm.depth<-nc.woa.Tm.depth$dim$LAT$vals	
+	data.woa.Tm.depth<-ncvar_get(nc.woa.Tm.depth, nc.woa.Tm.depth$var[[1]], start=c(which(lons.woa.Tm.depth==trans.lon),1), count=c(1, 180))
+	nc_close(nc.woa.Tm.depth)
+	names(data.woa.Tm.depth)<-lats.woa.Tm.depth
+	data.woa.Tm.depth2<-data.woa.Tm.depth*-1
+	
+	
+	file.woa.po2<-paste("/Data/WOA/WOA_o2/WOA_po2av_annual_1deg.nc", sep="")
+	nc.woa.po2<-nc_open(paste(file.woa.po2, sep=""))
+	lons.woa.po2<-nc.woa.po2$dim$LON$vals	
+	lats.woa.po2<-nc.woa.po2$dim$LAT$vals
+	depths.woa.po2<-nc.woa.po2$dim$DEPTH$vals		
+	data.woa.po2<-ncvar_get(nc.woa.po2, nc.woa.po2$var[[2]], start=c(which(lons.woa.po2==trans.lon),1,1), count=c(1, 180, 24))
+	nc_close(nc.woa.po2)
+	rownames(data.woa.po2)<-lats.woa.po2	
+	colnames(data.woa.po2)<-depths.woa.po2*-1
+	data.woa.po2<-data.woa.po2[,ncol(data.woa.po2):1]
+	
+	file.habitat.To<-paste("/Data/Projects/CMIP5_p50/IUCN/nc/IUCN_Thunnus_obesus.nc")
+	nc.habitat.To<-nc_open(paste(file.habitat.To, sep=""))
+	lons.habitat.To<-nc.habitat.To$dim$lon$vals
+#	lons.habitat.To<-ifelse(lons.habitat.To<0, lons.habitat.To+360, lons.habitat.To)		
+	lats.habitat.To<-nc.habitat.To$dim$lat$vals	
+	data.habitat.To<-ncvar_get(nc.habitat.To, nc.habitat.To$var[[1]], start=c(which(lons.habitat.To==-152.5),1), count=c(1, 180))
+	data.habitat.To<-data.habitat.To*-25
+
+	file.habitat.Tm<-paste("/Data/Projects/CMIP5_p50/IUCN/nc/IUCN_Thunnus_orientalis.nc")
+	nc.habitat.Tm<-nc_open(paste(file.habitat.Tm, sep=""))
+	lons.habitat.Tm<-nc.habitat.Tm$dim$lon$vals
+#	lons.habitat.Tm<-ifelse(lons.habitat.Tm<0, lons.habitat.Tm+360, lons.habitat.Tm)	
+	lats.habitat.Tm<-nc.habitat.Tm$dim$lat$vals	
+	data.habitat.Tm<-ncvar_get(nc.habitat.Tm, nc.habitat.Tm$var[[1]], start=c(which(lons.habitat.Tm==-152.5),1), count=c(1, 180))
+	data.habitat.Tm<-data.habitat.Tm*-25
+
+
+#-------------------------
+# Create Plot
+#-------------------------
+
+graphout<-paste("~/Code/Projects/CMIP5_p50/graphs/IUCN_WOA_PacificTransect.ps")
+postscript(graphout, width=5.5, height=7)
+	#quartz(width=5.5, height=7)
+	par(plt = c(0.17,0.75,0.60,0.95), #c(left, right, bottom, top)  
+    las = 1,                      # orientation of axis labels
+    cex.axis = 1,                 # size of axis annotation
+    tck = -0.04,
+    xaxs="i",
+    yaxs="i")
+
+#	filled.contour3(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)), ylab="Depth (m)", xlab="", lwd=1)
+	
+	filled.contour3(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, xlim=c(-70, 70), ylim=c(-1500,0), zlim=c(0,3), nlevels=10, color.palette=viridis, axes=FALSE, ylab="Depth (m)", xlab="", lwd=1)
+	contour(x=as.numeric(rownames(data.woa.po2)), y=as.numeric(colnames(data.woa.po2)), z=data.woa.po2, add=TRUE, nlevels=15, levels=c(1,2,5,10,15), col="white")
+	axis(side=1, at=seq(-70,70,20), labels=FALSE)
+	axis(side=2, at=seq(-1600,0,200), labels=TRUE)
+	axis(side=3, at=c(-70,70), lwd.ticks=0, labels=FALSE)
+	axis(side=4, at=c(-1500,0), lwd.ticks=0, labels=FALSE)
+	lines(as.numeric(names(data.woa.To.depth)), data.woa.To.depth2, col="#FF1493", lwd=2.5)
+	points(lats.habitat.To, data.habitat.To, pch=20, cex=0.3)
+	
+	par(new = "TRUE",
+    plt = c(0.8,0.85,0.60,0.95),   # define plot region for legend
+    las = 1,
+    cex.axis = 0.8,
+    tck=-0.4)
+
+#filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z= data.woa.To, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)))
+
+filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,3), nlevels=10, color.palette=viridis)
+filled.legend(x=as.numeric(rownames(data.woa.To)), y=as.numeric(colnames(data.woa.To)), z=data.woa.To, zlim=c(0,3), nlevels=10, color.palette=viridis)
+
+
+	par(new = "TRUE",
+	plt = c(0.17,0.75,0.15,0.5), #c(left, right, bottom, top)  
+    las = 1,                      # orientation of axis labels
+    cex.axis = 1,                 # size of axis annotation
+    tck = -0.04,
+    xaxs="i",
+    yaxs="i")
+
+#	filled.contour3(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)), ylab="Depth (m)", xlab="", lwd=1)
+
+	filled.contour3(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, xlim=c(-70, 70), ylim=c(-1500,0), zlim=c(0,10), nlevels=10, color.palette=viridis, axes=FALSE, ylab="Depth (m)", xlab="Degrees Latitude", lwd=1)
+	contour(x=as.numeric(rownames(data.woa.po2)), y=as.numeric(colnames(data.woa.po2)), z=data.woa.po2, add=TRUE, nlevels=15, levels=c(1,2,5,10,15), col="white")
+	axis(side=1, at=seq(-70,70,20), labels=TRUE)
+	axis(side=2, at=seq(-1600,0,200), labels=TRUE)
+	axis(side=3, at=c(-70,70), lwd.ticks=0, labels=FALSE)
+	axis(side=4, at=c(-1500,0), lwd.ticks=0, labels=FALSE)
+	lines(as.numeric(names(data.woa.Tm.depth)), data.woa.Tm.depth2, col="#FF1493", lwd=2.5)
+	points(lats.habitat.Tm, data.habitat.Tm, pch=20, cex=0.3)
+	
+	par(new = "TRUE",
+    plt = c(0.8,0.85,0.15,0.5),   # define plot region for legend
+    las = 1,
+    cex.axis = 0.8,
+    tck=-0.4)
+
+#	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z= data.woa.Tm, zlim=c(0,10), color.palette=colorRampPalette(viridis(20, begin=1, end=0)))
+
+	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color=viridis, nlevels=10)
+	filled.legend(x=as.numeric(rownames(data.woa.Tm)), y=as.numeric(colnames(data.woa.Tm)), z=data.woa.Tm, zlim=c(0,10), color=viridis, nlevels=10)
+
+dev.off()
--- RCode/IUCN_WOA_geostats_p50depth.R
+++ RCode/IUCN_WOA_geostats_p50depth.R
@@ -2,24 +2,23 @@
 # WOA Area with P50 Depth 
 #-------------------------------
 
-woa<-read.table("/Data/Projects/CMIP5_p50/WOA/WOA_p50depthav_area.txt")
+woa<-read.table("/Data/Projects/CMIP5_p50/IUCN_WOA/IUCN_WOA_p50depthav_area.txt")
 
 woa<-woa[,4:13]
-colnames(woa)<-c("experiment","species", "p50", "deltaH", "OceanArea", "P50Area", "P50Area_deltaH0", "P50DepthAv", "P50DepthVar", "P50DepthN")
-woa<-woa[,c("species", "p50", "deltaH", "P50Area", "P50Area_deltaH0")]
+colnames(woa)<-c("experiment","species", "p50", "deltaH", "OceanArea", "HabitatArea", "P50Area", "P50DepthAv", "P50DepthVar", "P50DepthN")
+woa<-woa[,c("species", "p50", "deltaH", "HabitatArea", "P50Area")]
 
 woa<-woa[order(woa$deltaH),]
 
 woa$P50Area<-woa$P50Area/1000000 #convert from m^2 to km^2
 woa$P50Area<-woa$P50Area/1000000 #convert to scale y-axis labels, ylab adds zeros
 
-woa$P50Area_deltaH0<-woa$P50Area_deltaH0/1000000 #convert from m^2 to km^2
-woa$P50Area_deltaH0<-woa$P50Area_deltaH0/1000000 #convert to scale y-axis labels, ylab adds zeros
+woa$HabitatArea<-woa$HabitatArea/1000000 #convert from m^2 to km^2
+woa$HabitatArea<-woa$HabitatArea/1000000 #convert to scale y-axis labels, ylab adds zeros
 
-woa<-woa[,c("P50Area", "P50Area_deltaH0")]
+woa<-woa[,c("P50Area", "HabitatArea")]
 
 woa<-as.matrix(t(woa))
-rownames<-c("deltaH ", )
 
 
 #-------------------------
@@ -28,11 +27,11 @@ rownames<-c("deltaH ", )
 
 library(ncdf)
 
-specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_alalunga","Thunnus_thynnus", "Thunnus_maccoyii")
+specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_alalunga","Thunnus_thynnus", "Thunnus_orientalis", "Thunnus_maccoyii")
 
 for(a in 1:length(specieslist)){
 	
-	file<-paste("/Data/Projects/CMIP5_p50/WOA/", specieslist[a], "/p50depth/woa.p50depthav.", specieslist[a], ".nc", sep="")
+	file<-paste("/Data/Projects/CMIP5_p50/IUCN_WOA/IUCN.WOA.p50depthav.", specieslist[a], ".nc", sep="")
 	nc<-open.ncdf(paste(file, sep=""))	
 		data<-get.var.ncdf(nc, nc$var[[1]], start=c(1,1), count=c(360, 180))
 		close.ncdf(nc)
@@ -54,166 +53,33 @@ for(a in 1:length(specieslist)){
 		
 	}
 depthtable<-depthtable*-1
-
-#-------------------------
-# P50 Depth WOA, deltaH0
-#-------------------------
-
-library(ncdf)
-
-specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_alalunga","Thunnus_thynnus", "Thunnus_maccoyii")
-
-for(a in 1:length(specieslist)){
-	
-	file<-paste("/Data/Projects/CMIP5_p50/WOA/", specieslist[a], "/p50depth/woa.p50depthav.", specieslist[a], ".nc", sep="")
-	nc<-open.ncdf(paste(file, sep=""))	
-		data<-get.var.ncdf(nc, nc$var[[2]], start=c(1,1), count=c(360, 180))
-		close.ncdf(nc)
-		data2<-as.vector(data)
-		data3<-subset(data2, is.na(data2)==FALSE)
-		if(a==1){
-			depthtable.H0<-as.matrix(data3)
-			colnames(depthtable.H0)<-specieslist[a]		
-		}else{
-			if(nrow(depthtable.H0)>length(data3)){
-				data3<-c(data3, rep(NA, 1, nrow(depthtable.H0)-length(data3)))					
-			}else{
-				addNA<-matrix(NA, length(data3)-nrow(depthtable.H0), ncol(depthtable.H0))
-				depthtable.H0<-rbind(depthtable.H0, addNA)				
-			}
-			depthtable.H0<-cbind(depthtable.H0, data3)
-			colnames(depthtable.H0)[a]<-paste(specieslist[a])
-		}	
-		
-	}
-depthtable.H0<-depthtable.H0*-1
-
-#-----------------------------------
-# Combine Depth Tables with Spacers
-#-----------------------------------
-addNA<-matrix(NA, nrow(depthtable)-nrow(depthtable.H0), ncol(depthtable.H0))
-depthtable.H0<-rbind(depthtable.H0, addNA)	
-
-cdepthtable<-cbind(depthtable[,"Thunnus_obesus"], depthtable.H0[,"Thunnus_obesus"], matrix(NA,nrow(depthtable),1), depthtable[,"Thunnus_albacares"], depthtable.H0[,"Thunnus_albacares"], matrix(NA,nrow(depthtable),1), depthtable[,"Katsuwonus_pelamis"], depthtable.H0[,"Katsuwonus_pelamis"], matrix(NA,nrow(depthtable),1), depthtable[,"Thunnus_alalunga"], depthtable.H0[,"Thunnus_alalunga"], matrix(NA,nrow(depthtable),1), depthtable[,"Thunnus_thynnus"], depthtable.H0[,"Thunnus_thynnus"], matrix(NA,nrow(depthtable),1), depthtable[,"Thunnus_maccoyii"], depthtable.H0[,"Thunnus_maccoyii"])
+depthtable[,5]<-rep(NA) #Thunnus thynnus has almost no habitat area with a P50 depth.
 
 
-#-------------------------
-# P50 Depth WOA Common Area
-#-------------------------
-
-library(ncdf)
-
-specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_alalunga","Thunnus_thynnus", "Thunnus_maccoyii")
-
-for(a in 1:length(specieslist)){
-	
-	file<-paste("/Data/Projects/CMIP5_p50/WOA/", specieslist[a], "/p50depth/woa.p50depth.common.", specieslist[a], ".nc", sep="")
-	nc<-open.ncdf(paste(file, sep=""))	
-		data<-get.var.ncdf(nc, nc$var[[1]], start=c(1,1), count=c(360, 180))
-		close.ncdf(nc)
-		data2<-as.vector(data)
-		data3<-subset(data2, is.na(data2)==FALSE)
-		if(a==1){
-			depthtable2<-as.matrix(data3)
-			colnames(depthtable2)<-specieslist[a]		
-		}else{
-			if(nrow(depthtable2)>length(data3)){
-				data3<-c(data3, rep(NA, 1, nrow(depthtable2)-length(data3)))					
-			}else{
-				addNA<-matrix(NA, length(data3)-nrow(depthtable2), ncol(depthtable2))
-				depthtable2<-rbind(depthtable2, addNA)				
-			}
-			depthtable2<-cbind(depthtable2, data3)
-			colnames(depthtable2)[a]<-paste(specieslist[a])
-		}	
-		
-	}
-depthtable2<-depthtable2*-1
-
-#-------------------------------------
-# P50 Depth WOA, deltaH0 Common Area
-#-------------------------------------
-
-library(ncdf)
-
-specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_alalunga","Thunnus_thynnus", "Thunnus_maccoyii")
-
-for(a in 1:length(specieslist)){
-	
-	file<-paste("/Data/Projects/CMIP5_p50/WOA/", specieslist[a], "/p50depth/woa.p50depth.common.", specieslist[a], ".nc", sep="")
-	nc<-open.ncdf(paste(file, sep=""))	
-		data<-get.var.ncdf(nc, nc$var[[2]], start=c(1,1), count=c(360, 180))
-		close.ncdf(nc)
-		data2<-as.vector(data)
-		data3<-subset(data2, is.na(data2)==FALSE)
-		if(a==1){
-			depthtable2.H0<-as.matrix(data3)
-			colnames(depthtable2.H0)<-specieslist[a]		
-		}else{
-			if(nrow(depthtable2.H0)>length(data3)){
-				data3<-c(data3, rep(NA, 1, nrow(depthtable2.H0)-length(data3)))					
-			}else{
-				addNA<-matrix(NA, length(data3)-nrow(depthtable2.H0), ncol(depthtable2.H0))
-				depthtable2.H0<-rbind(depthtable2.H0, addNA)				
-			}
-			depthtable2.H0<-cbind(depthtable2.H0, data3)
-			colnames(depthtable2.H0)[a]<-paste(specieslist[a])
-		}	
-		
-	}
-depthtable2.H0<-depthtable2.H0*-1
-
-#-----------------------------------
-# Combine Depth Tables with Spacers
-#-----------------------------------
-addNA<-matrix(NA, nrow(depthtable2.H0)-nrow(depthtable2), ncol(depthtable.H0))
-depthtable2<-rbind(depthtable2, addNA)	
-
-cdepthtable2<-cbind(depthtable2[,"Thunnus_obesus"], depthtable2.H0[,"Thunnus_obesus"], matrix(NA,nrow(depthtable2),1), depthtable2[,"Thunnus_albacares"], depthtable2.H0[,"Thunnus_albacares"], matrix(NA,nrow(depthtable2),1), depthtable2[,"Katsuwonus_pelamis"], depthtable2.H0[,"Katsuwonus_pelamis"], matrix(NA,nrow(depthtable2),1), depthtable2[,"Thunnus_alalunga"], depthtable2.H0[,"Thunnus_alalunga"], matrix(NA,nrow(depthtable2),1), depthtable2[,"Thunnus_thynnus"], depthtable2.H0[,"Thunnus_thynnus"], matrix(NA,nrow(depthtable2),1), depthtable2[,"Thunnus_maccoyii"], depthtable2.H0[,"Thunnus_maccoyii"])
-
-
-
-#quartz(height=8, width=4)
-outfile<-paste("~/Code/Projects/CMIP5_p50/graphs/WOA_geostats_zoom.ps")
-postscript(outfile, height=8, width=4)
-par(mfrow=c(3,1))
+#quartz(height=6, width=4)
+outfile<-paste("~/Code/Projects/CMIP5_p50/graphs/IUCN_WOA_geostats.ps")
+postscript(outfile, height=6, width=4)
+par(mfrow=c(2,1))
 par(mar=c(1, 4.5, 0, 0.5))
-par(oma=c(2, 0, 2, 1))
+par(oma=c(3.8, 0, 2, 1))
 par(las=1)
 par(yaxs="i")
 
-  collist<-c("#7fc97f", "white", "#beaed4", "white", "#fdc086", "white", "#ffff99", "white", "#386cb0", "white", "#f0027f", "white")
+  collist<-c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17")
 
-  barplot(woa, ylim=c(0, 200), col=collist, legend=c(expression(paste(Delta, "H", "\U2260", "0 (Effect of Temperature)", sep=""), paste(Delta, "H", "\U003D", "0 (No Effect of Temperature)", sep=""))), args.legend = list(x = "topleft", inset=c(0.05, 0.02), cex=1), xaxt="n", ylab=expression(paste("area (10"^"6", " km"^"2", ")", sep="")), space=c(0,0.8), beside=TRUE)
+  barplot(woa[2,], ylim=c(0, 300), col="grey", legend=c("P50 Depth", "Habitat"), args.legend = list(x = "topleft", fill=c("black", "grey"), inset=c(0.05, 0.02), cex=0.8), xaxt="n", ylab=expression(paste("area (10"^"6", " km"^"2", ")", sep="")), space=0.8)
+  barplot(woa[1,], ylim=c(0, 300), col=collist, xaxt="n", space=0.8, add=TRUE)
   box()
   abline(h=0, lwd=2)
   
-   
-  collist<-c("#7fc97f", "white","", "#beaed4", "white", "", "#fdc086", "white", "", "#ffff99", "white", "", "#386cb0", "white", "", "#f0027f", "white")
-  medcollist<-c("black", "black", "","black", "black", "","black", "black", "","black", "black", "","black", "black", "","black", "black")
-  
-    collist_border<-c("black","#7fc97f", "","black", "#beaed4",  "", "black", "#fdc086",  "", "black","#ffff99",  "","black", "#386cb0",  "", "black", "#f0027f")
-    
-  
-#  collist<-c("black", "white","", "black", "white", "", "black", "white", "", "black", "white", "", "black", "white", "", "black", "white")
-#  medcollist<-c("white", "black", "","white", "black", "","white", "black", "","white", "black", "","white", "black", "","white", "black")
-  
-  
-#  boxplot(cdepthtable, ylim=c(-1500, 0), xaxt="n", ylab="depth (m)", col=collist, medcol=medcollist, notch=TRUE, outline=FALSE)
-#  locs<-c(1,2,4,5,7,8,10,11,13,14,16,17)
-#  axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-  
-  boxplot(cdepthtable2, ylim=c(-1500, 0), xaxt="n", col=collist, medcol=medcollist, ylab="depth (m) for common areas", notch=TRUE, outline=FALSE)
+  locs<-seq(0.7,8.7,1.2)
+   boxplot(depthtable, ylim=c(-1500, 0), xaxt="n", col=collist, ylab="depth (m)", notch=TRUE, outline=FALSE, at=locs)
   axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-  specieslist<-c("Thunnus\nobesus", "Thunnus\nalbacares", "Katsuwonus\npelamis", "Thunnus\nalalunga", "Thunnus\nthynnus",  "Thunnus\nmaccoyii")
-  axis(side=1, at=c(1.5,4.5,7.5,10.5,13.5,16.5), line=-0.5, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8)
-  mtext("WOA P50Depth", adj=0.6, outer=TRUE)
+
+  specieslist<-c("T. obesus", "T. albacares", "K. pelamis", "T. alalunga", "T. thynnus", "T. orientalis",  "T. maccoyii")
+  axis(side=1, at=locs, line=-1, las=2, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8)
+#  mtext("WOA P50Depth", adj=0.65, outer=TRUE)
   
-  boxplot(cdepthtable2, ylim=c(-500, 0), xaxt="n", col=collist, medcol=medcollist, ylab="depth (m) for common areas", notch=TRUE, outline=FALSE)
-  axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-  specieslist<-c("Thunnus\nobesus", "Thunnus\nalbacares", "Katsuwonus\npelamis", "Thunnus\nalalunga", "Thunnus\nthynnus",  "Thunnus\nmaccoyii")
-  axis(side=1, at=c(1.5,4.5,7.5,10.5,13.5,16.5), line=-0.5, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8)
-  mtext("WOA P50Depth", adj=0.6, outer=TRUE)
 dev.off()
 
 
