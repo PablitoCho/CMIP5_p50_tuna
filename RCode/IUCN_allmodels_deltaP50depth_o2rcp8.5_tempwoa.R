cb7ec66cc48b20d06e099472dba0de520c2a4943
kallisons <kallisonsmith@yahoo.com>
2017-03-06 13:36:28 -0800

    Moved axis labels

--- RCode/IUCN_allmodels_deltaP50depth_o2rcp8.5_tempwoa.R
+++ RCode/IUCN_allmodels_deltaP50depth_o2rcp8.5_tempwoa.R
@@ -64,25 +64,16 @@ depthtable_Thist<-depthtable_Thist[,c("Thunnus_obesus", "Thunnus_albacares", "Ka
 #------------------------
 
 outfile<-paste("~/Code/Projects/CMIP5_p50/graphs/IUCN_deltadepth_modelmean.ps")
-#postscript(outfile, height=5.5, width=4, family="Times")
-quartz(height=5.5, width=5)
+postscript(outfile, height=5.5, width=5, family="Times")
+#quartz(height=5.5, width=5)
 par(mfrow=c(2,1))
 par(mar=c(1, 4.5, 1, 3.5))
 par(oma=c(3.8, 0, 1, 1))
 par(las=1)
 par(yaxs="i")
 par(tck = 0.03) 
-#collist<-c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17")
+par(mgp = c(2,0.3,0))
 collist<-c("#1b9e77", "#d95f02", "#d95f02", "#7570b3", "#7570b3", "#7570b3", "#7570b3")
-
-#barplot(areatable_means, ylim=c(-2, 10), col=collist, xaxt="n", ylab=expression(paste("change in area (10"^"6", " km"^"2", ")", sep="")), beside=TRUE)
-#locs<-seq(0.7,8.7,1.2)
-#arrows(locs,areatable_means, locs, up_se, lwd=2, length=0.1, angle=90)
-#arrows(locs,areatable_means, locs, down_se, lwd=3, length=0.1, angle=90, col="white")
-#box()
-#locs<-seq(0.7,8.7,1.2)
-#axis(side=1, at=locs, labels=FALSE, tick=TRUE)
-#abline(h=0)
 
 locs<-seq(0.7,6.5,1.2)
 vlocs<-c(1.3, 3.7)
@@ -94,7 +85,7 @@ boxplot(depthtable, col=collist, xaxt="n", yaxt="n", at=locs, notch=TRUE, outlin
 altaxis<-c(-600,-300,0,300,600)
 converttcks<-altaxis/3.2
 axis(side=4, at=converttcks, line=-3.5, labels=altaxis, tick=TRUE, outer=TRUE, lwd=1, cex.axis=0.8, tck=0.03, adj=0.5)
-mtext("Change in depths (m)", side=4, at=0.2, las=0, line=3, adj=0.5)
+mtext("Change in depths (m)", side=4, at=0.2, las=0, line=2, adj=0.5)
 abline(v=vlocs[1], lty=3, lwd=1.5)
 abline(v=vlocs[2], lty=3, lwd=1.5)
 segments(4.9, -60, 4.9, 250, lty=1)
@@ -114,9 +105,9 @@ axis(side=1, at=locs, labels=FALSE, tick=TRUE, tck=0.03)
 abline(h=0, lwd=1)
 boxplot(depthtable_Thist, col=collist, xaxt="n", yaxt="n", ylim=c(-400, 800), at=locs, notch=TRUE, outline=FALSE, add=TRUE, lty=1)
 specieslist<-c("bigeye", "yellowfin", "skipjack", "P. bluefin", "S. bluefin")
-axis(side=1, at=locs, line=-1.7, las=2, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8, tck=0.03)
+axis(side=1, at=locs, line=-1, las=2, labels=specieslist, tick=FALSE, outer=TRUE, cex.axis=0.8, tck=0.03)
 axis(side=4, at=converttcks, line=-3.5, labels=altaxis, tick=TRUE, outer=TRUE, lwd=1, cex.axis=0.8, tck=0.03, adj=0.5)
-mtext("Change in depths (m)", side=4, at=0.2, las=0, line=3, adj=0.5)
+mtext("Change in depths (m)", side=4, at=0.2, las=0, line=2, adj=0.5)
 abline(v=vlocs[1], lty=3, lwd=1.5)
 abline(v=vlocs[2], lty=3, lwd=1.5)
 segments(4.9, -60, 4.9, 250, lty=1)
@@ -128,13 +119,16 @@ text(1.6, -200,"independent", cex=0.8, pos=4)
 text(4, -200,"endothermic", cex=0.8, pos=4)
 mtext("(b)", side=3, at=0.2)
 mtext("ocean surface", side=3, las=1, at=5.4, cex=0.8)
-#dev.off()
+dev.off()
 
 #--------------------------
 # Calculate Statistics
 #---------------------------
 Median_O2Temp<-apply(depthtable, MARGIN=2, FUN=median, na.rm=TRUE)
 Median_O2<-apply(depthtable_Thist, MARGIN=2, FUN=median, na.rm=TRUE)
+
+Median_O2Temp[5]<-Median_O2Temp[5]*3.2
+Median_O2[5]<-Median_O2[5]*3.2
 
 Median_O2Temp-Median_O2
 
