9a29cecdacde467b1e4b7245bf3a7e74b82c98e4
kallisons <kallisonsmith@yahoo.com>
2017-03-06 13:17:38 -0800

    Delete unused legacy code

--- RCode/IUCN_allmodels_deltaP50depth_o2rcp8.5_tempwoa.R
+++ RCode/IUCN_allmodels_deltaP50depth_o2rcp8.5_tempwoa.R
@@ -1,53 +1,3 @@
-
-#-------------------------------
-# Change in Area with P50 Depth 
-#-------------------------------
-#models<-c("cesm1", "esm2g", "esm2m", "hadgem2", "ipsl", "mpi")
-#folder<-paste("/Data/Projects/CMIP5_p50/IUCN_models/")
-
-#area.all<-as.data.frame(matrix(NA, 0, 7))
-
-#for(a in 1:length(models)){
-
-#woa<-read.table("/Data/Projects/CMIP5_p50/IUCN_WOA/IUCN_WOA_p50depthav_area.txt")
-#rcp<-read.table(paste(folder, "/IUCN_", models[a], "_p50depthav_area_rcp8.5.txt", sep=""))
-
-#woa<-woa[,4:13]
-#colnames(woa)<-c("experiment","species", "p50", "deltaH", "OceanArea", "HabitatArea", "P50Area", "P50DepthAv", "P50DepthVar", "P50DepthN")
-#woa<-woa[,c("species", "p50", "deltaH", "P50Area")]
-#colnames(woa)[4]<-"woa"
-
-#rcp<-rcp[,4:13]
-#colnames(rcp)<-c("experiment","species", "p50", "deltaH", "OceanArea", "HabitatArea", "P50Area", "P50DepthAv", "P50DepthVar", "P50DepthN")
-#rcp<-rcp[,c("species", "p50", "deltaH", "P50Area")]
-#colnames(rcp)[4]<-"rcp"
-#rcp$model<-rep(models[a])
-
-#area<-merge(woa, rcp, all=TRUE)
-#area<-area[order(area$deltaH),]
-
-#area$change<-area$rcp-area$woa
-
-#area$change<-area$change/1000000 #convert from m^2 to km^2
-#area$change<-area$change/1000000 #convert to scale y-axis labels, ylab adds zeros
-
-#area.all<-rbind(area.all, area)
-#}
-
-#specieslist<-unique(area.all[,c("species", "deltaH")])
-#specieslist<-specieslist[order(specieslist$deltaH),]
-#specieslist<-specieslist$species
-
-#for(d in 1:length(specieslist)){
-#	one<-subset(area.all, area.all$species==specieslist[d])
-#	one<-one[,c("model", "change")]
-#	if(d==1){areatable<-one}else{areatable<-cbind(areatable,one[,2])}
-#	colnames(areatable)[d+1]<-specieslist[d]
-#}
-#rownames(areatable)<-areatable$model
-#areatable<-areatable[,-1]
-#areatable<-as.matrix(areatable)
-
 #-----------------------------
 # Average Change in P50 Depth
 #-----------------------------
@@ -108,17 +58,6 @@ specieslist<-c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thu
 #Thunnus thynnus has no habitat area with a P50 depth.
 depthtable_Thist<-depthtable_Thist[,c("Thunnus_obesus", "Thunnus_albacares", "Katsuwonus_pelamis", "Thunnus_orientalis", "Thunnus_maccoyii")]
 
-
-#--------------------------
-# Calculate Area Statistics
-#---------------------------
-
-#areatable_means<-apply(areatable, MARGIN=2, FUN=mean)
-#areatable_sd<-apply(areatable, MARGIN=2, FUN=sd)
-#areatable_se<-areatable_sd/sqrt(6)
-
-#up_se<-areatable_means+areatable_se
-#down_se<-areatable_means-areatable_se
 
 #------------------------
 # Plot P50 Depth Changes 
